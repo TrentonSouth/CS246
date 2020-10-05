7453ea01aad70a60da0ff4f65a392d60280f4104
hig19011 <59715793+hig19011@users.noreply.github.com>
2020-10-05 16:36:27 -0700

    Fixed name

--- Hello/src/trenton/Main.java
+++ Hello/src/trenton/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Trenton's Document");
-	    System.out.println("Hello Christine, my name is Gene.");
+	    System.out.println("Hello Trenton, my name is Gene.");
 	    
     }
 }
