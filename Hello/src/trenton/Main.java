8d2d45ff318c4c96a7690f97c56a199ac6d525a5
NoonanChristine <65037707+NoonanChristine@users.noreply.github.com>
2020-10-05 19:42:05 -0400

    Christine's update to main

--- Hello/src/trenton/Main.java
+++ Hello/src/trenton/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Trenton's Document");
 	    System.out.println("Hello Trenton, my name is Gene.");
-	    
+	    System.out.println("Hi Trenton!  Christine here.");
     }
 }
